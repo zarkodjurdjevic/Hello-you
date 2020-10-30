95426079a866ad39bfa7c8a0363bf63e5c37c699
unknown <31924@ma-web.nl>
2020-10-30 12:00:54 +0100

    einde

---
+++ textbased opdracht nieuwkomer.py
@@ -0,0 +1,259 @@
+print("Deze applicatie gaat over en 37 jarige man genaamd Adam die 4 jaar geleden is gevlucht naar nederland, ")
+print("Adam woonde ze hele leven in Syrie met zijn vrouw en 2 kinderen naar zijn mening was het daar ook fijn leven voor hun want ze hadden en mooie woning, mooie auto en een goede baan.")
+print("Daarom ga jij nu het verhaal beleven van Adam en kan je zelf kiezen wat je zou doen in bepaalde situaties en welke keuzes je zou maken.")
+print("je krijgt de mogelijkheid om te kiezen uit ")
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("Er zijn de laatste tijd veel demonstraties in Syrie op bepaalde plekken. De 2 broers van Adam doen ook mee.")
+print("Adam besluit een wandeling te maken en voor zijn deur ziet hij veel demonstranten die hard worden aangepakt door de")
+print("politie, het geweld is afschuwelijk je besluit daarom in te grijpen kies daarom op welke manier je zou willen ingrijpen.")
+print("A = probeer in te grijpen en het op te nemen tegen de politie.")
+print("B = schrijf een artikel over het slechte en corrupte bestuur van het land.")
+print("C = maak en gedicht en probeer aan de hand daarvan het bestuur belachelijk te maken.")
+antwoord = input()
+print("#################################################################################################################")
+if antwoord == "B" or antwoord == "b" or antwoord == "C" or antwoord == "c":
+    print("in Syrie word kritiek op het bestuur en het belachelijk maken van het bestuur niet geaccepteerd dus daarom werd er als waarschuwing bij Adam en brief op zijn deur geplakt met daarop je gaat eraan.")
+    print("Adam schrok ervan dus die zat te denken over wat slim is om te doen dus kies daarom uit de onderstaande keuzes.")
+    print("A = je er niks van aantrekken en gewoon je normale leven leiden.")
+    print("B = probeer te vluchten naar een ander land die je goede kansen bied op gebied van werk, wonen enzovoort.")                                                     
+    antwoord = input()
+    if antwoord == "A" or antwoord == "a":
+        print("DAT WAS GEEN VERSTANDIGE KEUS JE BENT GEPAKT!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("THE END")
+    if antwoord == "B" or antwoord == "b":
+        print("Je besluit te vluchten maar dan moet je eerst alles in orde maken en spullen klaarmaken voor het vertrek want je hebt niet heel veel tijd tot dat ze je komen zoeken.")
+        print("Je ziet tijdens het klaar zetten van je spullen dat je paspoort verlopen is. neem een verstandige beslissing uit de onderstaande keuzes over wat jij in zo en situatie zou doen.")
+        print("A = probeer onherkenbaar je paspoort te verlengen en zo snel mogelijk.")
+        print("B = probeer het land te verlaten via en illegale en onherkenbare route.")
+        print("C = probeer met een neppe paspoort het land te verlaten het is risicovol maar wel veel sneller.")
+        antwoord = input()
+        if antwoord == "A" or antwoord == "a":
+                print("je bent je paspoort gaan verlengen maar de vrouw die je hulp gaf met het verlengen had je herkent maar gelukkig zij de vrouw dat ze het niet zou melden omdat ze vond dat je gelijk had.")
+                print("zodra je naar buiten liep zaggen 2 agenten je van de schrik liet je, je paspoort vallen en rende zo snel mogelijk weg. je bent ze uit eindelijk kwijt geraakt maar je beseft je dat je, je paspoort hebt laten vallen.")
+                print("de enigste mogelijkheid die er over blijft is het vluchten op en illegale manier en zo onherkenbaar mogelijk.")
+        elif antwoord == "B" or antwoord == "b":
+                print(".")
+        else:
+            print("je probeerd via via contact op te nemen met iemand die een neppe paspoort kan regelen. uiteindelijk vind je iemand maar je krijgt te horen dat het langer dan en week gaat duren tot dat je het binnen krijgt.")  
+            print("dat betekend dat er nog een mogelijkheid over is en dat is het land verlaten op illegale manier en via onherkenbare routes.")
+else:
+    print("De politie heeft je hard aangepakt en in de gevangenis gezet")
+    print("######################")
+    print("#                    #")
+    print("#      THE END       #")
+    print("#                    #")
+    print("######################")
+
+print("################################################################################################################################################")
+print("je gaat nu kijken welke manier en locatie het beste zijn voor het vluchten naar een ander land. je wilt het liefst in europa komen maar in eerste instantie is het belangrijk om weg te komen uit syrie.")
+print("adam kiest ervoor om naar Turkije te gaan omdat het beste en slimste is maar omdat je geen paspoort hebt gaat het binnenkomen van Turkije moeilijk worden dus ga je een gevaarlijke route moeten volgen.")
+print("door besneeuwde bergen. Het is natuurlijk niet fijn om zelf de reis af te leggen dus kijk je op internet of er groepen mensen zijn waarbij je, je kan aansluiten. je hebt en groep mensen gevonden en je bij hun aangesloten.")
+print("ze hebben vermeld dat je word opgehaald door hun in en klein busje vervolgens worden jullie gedropt bij het punt waar jullie te voet verder moeten.")
+print("###################################################################################################################")
+print("Na 3 uur gereden te hebben zijn jullie aangekomen bij het punt en moeten jullie nog 5 uur te voet gaan om Turkije binnen te komen. op bepaalde plekken op de berg waar jullie de route af moeten leggen ligt er veel sneeuw dus bereid je goed voor")
+print("sommige mensen besluiten te slapen omdat het al nacht is en sommige besluiten gelijk te vertrekken kies daarom wat jij zou doen.")
+print("A = overnachten en dan met de ene gedeelte van de groep gaan")
+print("B = gelijk vertrekken met de andere groep")
+print("C = doe alles op eigen tempo")
+antwoord = input()
+if antwoord == "a" or antwoord == "A":
+    print("Na en nachtje goed uitgerust te hebben besluiten jij en het groepje te vertrekken.")
+    print("Na 2 uur lopen zijn veel mensen moe en besluiten ze hun tenten te plaatsen om even uit te rusten en in de ochtend weer verder te gaan")
+    print("maar en klein gedeelte besluit verder te lopen.")
+    print("kies wat jij in dat geval zou doen.")
+    print("A = plaats je tent en ga weer in de ochtend verder op klaarlichte dag.")
+    print("B = ga verder met het kleine groepje die er niet voor kiest om uit te rusten maar die gelijk door willen gaan.")
+    antwoord = input()
+    if antwoord == "A" or antwoord == "a":
+        print("nadat iedereen heeft uitgerust in hun tent besluit iedereen vroeg in de ochtend te vertrekken.")
+    elif antwoord == "B" or antwoord == "b":
+        print("je hebt geen rust nodig dus besluit door te gaan.")
+    
+
+elif antwoord == "B" or antwoord == "b":
+    print("je vertrekt gelijk met het groepje want je wilt er natuurlijk zo snel mogelijk zijn")
+else:
+    print("je besluit alles op eigen tempo te doen en pas uit te rusten als je het zelf wilt.")
+
+print("met nog 30 minuten lopen hoor je van veel mensen dat er pas morgen een boot vertrekt naar Lesbos dat is een eiland in de buurt van Turkije waar vluchtenlingen worden opgevangen")
+print("en daar in en kamp kunnen komen en vervolgens een visum aanvragen voor griekenland")
+print("#############################################################################################################################")
+print("bij aankomst worden jullie opgehaald door en bus en dan worden jullie afgezet bij de locatie waar jullie morgen op de boot moeten stappen, het enigste nadeel is dat jullie en dag moeten wachten op de boot.")
+print("je mist je vrouw en 2 kinderen heel erg maar je besluit ze toch niet te bellen want anders kunnen ze in gevaar komen.")
+print("je vraagt je af of alles wel veilig is op de boot naar Lesbos dus besluit de kapitein te vragen of alles veilig is, de kapitein zegt dat we een hele grote risico lopen omdat we met heel veel mensen gepropt op de boot gaan zitten.")
+print("je schrikt toch wel en beetje van zijn uitspraak neem daarom de keuze of je het moet bertellen tegen de mensen die allemaal meegaan.")
+print("A = besluit mensen hier over in te lichten")
+print("B = besluit het toch maar voor je te houden om geen paniek te veroorzaken.")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("veel mensen wisten al dat dit het geval zou zijn maar sommige zijn best bang geworden en zijn op het moment best gestrest")
+elif antwoord == "B" or antwoord == "b":
+    print("is toch de slimste keus om mensen niet af te schrikken.")
+print("Er zijn 2 boten aangekomen om de mensen af te zetten op Lesbos kies welke boot je zou nemen.")
+print("A = De grotere boot met wat meer mensen")
+print("B = De kleinere boot met wat minder mensen erop.")
+antwoord = input()
+print("De reis op de boot verliep goed en jullie zijn aangekomen op het eiland Lesbos. bij aankomst kregen jullie allemaal producten zoals dekkens, tandenborstels en extra kleren van de hulpdiensten die daar altijd klaar staan.")
+print("Bij aankomst werden jullie ook gelijk in een klein huisje gezet zodat iedereen zich even op kon frissen. er werd ook eten aangeboden kies wat jij op dat moment zou kiezen om te eten.")
+print("A = bak met sla")
+print("B = Patat en burger")
+print("C = Biefstuk")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("na heerlijk je bak sla opgegeten te hebben")
+elif antwoord == "B" or antwoord == "b":
+    print("na heerlijk je patat en burger opgegeten te hebben")
+else:
+    print("na heerlijk genoten te hebben van je biefstuk")
+print("word iedereen klaar gemaakt voor het vetrek naar de vluchtenlingenkamp die 30 minuten van jullie vandaan is. Je hoopt bij aankomst gelijk een visum aan te kunnen vragen maar dat gaat moeilijk omdat er daar nog veel meer")
+print("mensen zijn die een visum aan willen vragen voor Griekeland. Je gaat ongeveer een week in het kamp moeten blijven tot dat je het visum aan kan vragen.")
+print("Maar in die tijd worden veel activiteiten gedaan met mensen zodat mensen zich meer op hun gemak voelen en in iedergeval wat te doen hebben tot dat de visum geaccepteerd is.")
+print("Na en dag in het kamp besluit je een wandeling te maken door het kamp, je ziet een groepje mensen die aan het voetballen zijn, groepje mensen die aan het basketballen zijn en groepje mensen die aan het trefballen zijn")
+print("kies aan welke activiteit je zou meedoen.")
+print("A = voetbal")
+print("B = basketbal")
+print("C = trefbal")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("na heerlijk gevoetbalt te hebben besluit je weer verder te gaan met je wandeling.")
+if antwoord == "B" or antwoord == "b":
+    print("na heerlijk gebasketbalt te hebben besluit je weetr verder te gaan met je wandeling")
+if antwoord == "C" or antwoord == "c":
+    print("na heerlijk getrefbalt te hebben besluit je weer verder te gaan met je wandeling.")
+print("bij de wandeling zie je veel mensen waarvan je weet dat ze hier lang zitten dus besluit je een gesprek aan te gaan met iemand over hoe het al die tijd hier is")
+print("een man vertelt je dat het in de winter heel zwaar is en dat mensen soms ook heel ziek worden door de kou. hoe zou jij reageren als je zag hoe het in het kamp ging tijdens de winter.")
+print("A = Je zou zo snel mogelijk weg willen komen")
+print("B = Je zou mogelijkheden zoeken om mensen te helpen en jezelf")
+antwoord = input()
+print("begrijpelijk antwoord")
+print("je hoort ook dat sommige mensen het heel slecht vinden hier en dat ze liever terug zouden gaan. wat zou jij hierover vinden.")
+print("A = ik ben het ermee eens en zou precies het zelfde willen")
+print("B = ben het er niet mee eens want ik blijf liever hier")
+antwoord = input()
+print("veel mensen zouden het met je eens zijn.")
+print("ook hoor je dat mensen niet zomaar uit en in de kamp kunnen komen, wat zou jij hiervan vinden.")
+print("A = niet eerlijk want het voelt dan net alsof je en gevangenen bent.")
+print("B = eigenlijk wel goed want mensen zonder vergunning mogen niet zomaar in en bepaald land of gebied zonder documentatie.")
+print("C = heel verdrietig")
+antwoord = input()
+print("Veel mensen zouden het met je eens zijn")
+print("###########################################################################################################################################################")
+print("En tijd geleden was er soms niet genoeg voedsel en drinken veel mensen konden en dag of 2 geen voedsel krijgen, hoe zou jij daarop reageren")
+print("A = bang zijn hoe het verder gaat aflopen")
+print("B = niet bang zijn en hopen op het beste")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("Het is natuurlijk heel schrikwekkend als je hoort dat er een schaarste is aan voedsel en drinken")
+elif antwoord == "B" or antwoord == "b":
+    print("Hoewel het schrikwekkend is staat er toch niks anders op het spel dan hopen en proberen er een goeie tijd van te maken.")
+print("De man vertelt ook hoewel er soms hele zware omstandigheden zijn dat mensen er toch het beste uit willen halen en dat mensen bij elkaar komen om elkaar te steunen in zo en moeilijke periode.")
+print("Wat vind je van deze uitspraak.")
+print("A = Het maakt je blij dat mensen voor elkaar klaar staan in zo en moeilijke periode.")
+print("B = je hebt toch en beetje medelijden dat die mensen dit mee moeten maken.")
+print("C = je word er emotioneel van.")
+antwoord = input()
+print("Je besluit je wandeling af te sluiten en naar je huisje gaan om te gaan slapen.")
+print("Op de terug weg zie je nog een klein groepje nog een voetballen voor het slapen kies je ervoor om mee te doen.")
+print("A = MEEDOEN")
+if antwoord == "A" or antwoord == "a":
+    print("na heerlijk uitgeput te zijn kies je ervoor om naar je huisje te gaan om even te slapen en uit te rusten.")
+    print("je word vaker en je krijgt te horen dat je visum over 20 uur word geaccepteerd je besluit daarom om nog even paar activiteiten te doen en afscheid te nemem")
+    print("van de mensen waar je de afgelopen week goed bevriend mee was.")
+
+
+print("B = NIET MEEDOEN")
+if antwoord == "B" or antwoord == "b":
+    print("Je besluit het vandaag even over te slaan om goede slaap en rust te kunnen pakken.")
+    print("je word vaker en je krijgt te horen dat je visum over 20 uur word geaccepteerd je besluit daarom om nog even paar activiteiten te doen en afscheid te nemem")
+    print("van de mensen waar je de afgelopen week goed bevriend mee was.")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
